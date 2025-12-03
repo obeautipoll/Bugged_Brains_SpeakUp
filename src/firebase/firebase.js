a8dd27498641fde9c7f15f0b0109fd5dfb467143
maya. <alimarhamah9@gmail.com>
2025-12-04 01:44:10 +0800

    .env added

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqRfeSLKkuykc-vNkKLbvQHUIpZ9IaSU0",
-  authDomain: "bb-speakup.firebaseapp.com",
-  projectId: "bb-speakup",
-  storageBucket: "bb-speakup.firebasestorage.app",
-  messagingSenderId: "609466283176",
-  appId: "1:609466283176:web:2069d8971593fd0ab03e9d",
-  measurementId: "G-6L0J9Z5PS0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
